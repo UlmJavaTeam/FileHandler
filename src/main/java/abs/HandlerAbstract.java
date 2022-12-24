7bfde12efe9824e80d6d0e328f6b68e074150703
OzanKilic6 <kilic.ozan@outlook.de>
2022-12-24 21:47:30 +0100

    Initial commit

---
+++ src/main/java/abs/HandlerAbstract.java
@@ -0,0 +1,14 @@
+public abstract class HandlerAbstract {
+    private String text;
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String toString() {
+        return this.text;
+    }
+
+    public abstract Object parse();
+
+}
